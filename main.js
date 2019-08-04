e3355bb9e3442e97c220dcf1b7e49b995bf9a122
Gaston Guillaux <gaston.guillaux@gmail.com>
2019-08-04 00:24:39 -0300

    template updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 
-require('electron-reload')(__dirname)
+//require('electron-reload')(__dirname)
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
