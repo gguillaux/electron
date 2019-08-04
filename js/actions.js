0f15d0e012c8a00066ee542ec14415ae3db23831
Gaston Guillaux <gaston.guillaux@gmail.com>
2019-08-04 00:23:18 -0300

    template completed

---
+++ js/actions.js
@@ -0,0 +1,81 @@
+function clean() {
+  document.getElementById('description').innerHTML = "";
+  document.getElementById('data').innerHTML = "";
+}
+
+// ----------------------------------------------------------
+
+function loadHome() {
+  document.getElementById('header').innerHTML = "You have clicked the HOME button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadDownload() {
+  document.getElementById('header').innerHTML = "You have clicked the DOWNLOAD button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadMoney() {
+  document.getElementById('header').innerHTML = "You have clicked the MONEY button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadWorld() {
+  document.getElementById('header').innerHTML = "You have clicked the WORLD button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadBarChart() {
+  document.getElementById('header').innerHTML = "You have clicked the BARCHART button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadList() {
+  document.getElementById('header').innerHTML = "You have clicked the LIST button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadDatabase() {
+  document.getElementById('header').innerHTML = "You have clicked the DATABASE button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadSearch() {
+  document.getElementById('header').innerHTML = "You have clicked the SEARCH button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadPlane() {
+  document.getElementById('header').innerHTML = "You have clicked the PLANE button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadCode() {
+  document.getElementById('header').innerHTML = "You have clicked the CODE button!";
+  clean();
+}
+
+// ----------------------------------------------------------
+
+function loadSecretAgent() {
+  document.getElementById('header').innerHTML = "You have clicked the JAMES BOND button!";
+  clean();
+}
